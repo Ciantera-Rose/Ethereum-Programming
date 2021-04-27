6b34e96fad35f15d719817430132ef12760367e2
Ciantera-Rose <ciantera.rose@gmail.com>
2021-04-27 17:50:18 -0400

    edit compiler version

--- government.sol
+++ government.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.5;
+pragma solidity ^0.7.0;
 
 /* 
     This contract will save reports of what the bank contract is doing
--- hello-world.sol
+++ hello-world.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.4;
+pragma solidity 0.7.5;
 
 // Write a function is solidity and return HelloWorld
 // To get a response from the contract we need to write functions that return a statement
